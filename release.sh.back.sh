4f7bb9e98551e442c3aa7c84da73453ac09b3a7f
Mohammed Loukili <mloukili@bluenimble.com>
2022-06-08 10:34:09 -0500

    Update POMs for Release 2.52.0

--- release.sh.back.sh
+++ release.sh.back.sh
@@ -16,6 +16,12 @@ then
   exit 1
 fi
 
+if [ -z "$NextRelease" ]
+then
+  echo "Set Next Release eg. 1.3 " >&2
+  exit 1
+fi
+
 # Update POMs - Remove -SNAPSHOT
 java -cp assets/utilities/release.jar com.bluenimble.platform.release.Replace pom.xml "<version>$Release.0-SNAPSHOT</version><!--bn.version-->" "<version>$Release.0</version><!--bn.version-->"
 
@@ -27,7 +33,14 @@ git commit -m "Update POMs for Release $Release.0"
 git push origin master
 
 # Build Server and CLI
+mvn clean deploy
 mvn clean install
+
+# Build Broker
+cd tools/bluenimble-broker
+mvn clean install
+
+cd ../..
 
 # Create branches
 git checkout -b $Release
@@ -43,8 +56,14 @@ git add .
 git commit -m "Update POMs for next snapshot $NextRelease.0-SNAPSHOT"
 git push origin master
 
+cd build
+# remove netty-socketio
+rm bluenimble-broker-$Release.0/lib/netty-socketio-1.7.17.jar
+
 tar -czf bluenimble-$Release.0-bin.tar.gz bluenimble-$Release.0
 tar -czf bluenimble-cli-$Release.0-bin.tar.gz bluenimble-cli-$Release.0
+tar -czf bluenimble-broker-$Release.0-bin.tar.gz bluenimble-broker-$Release.0
 
 zip -r bluenimble-$Release.0-bin.zip bluenimble-$Release.0
 zip -r bluenimble-cli-$Release.0-bin.zip bluenimble-cli-$Release.0
+zip -r bluenimble-broker-$Release.0-bin.zip bluenimble-broker-$Release.0
